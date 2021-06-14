282a265badc84e298c3c48a1940ab8a13e6a1639
AngelDOrtiz <aortizghs@gmail.com>
2021-06-14 13:34:09 -0700

    changes made Sarah

--- data/setup.js
+++ data/setup.js
@@ -1,12 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { promises as fs } from 'fs';
-import path from 'path';
 
 export default (pool) => {
   return fs
     .readFile(
-      'C:/Users/Nessa/alchemy/Career-Track-Labs/CT-CRUD-Routes-04/sql/setup.sql',
+      './sql/setup.sql',
       {
         encoding: 'utf-8',
       }
